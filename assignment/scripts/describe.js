dc53f8d6ce2e50276ce8a5efdd2dd4c2cd6c1d6e
Andrew Harasymiw <aharasymiw@gmail.com>
2023-02-24 10:03:04 -0800

    add in descriptive comments about what each line of code does for part 1.  For part two, add in comments about how to fix the code.

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -26,10 +26,11 @@ if (number >= 2) {
 
 
 // 1. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+//    We make a variable called name and set it to Dane.
+//    Then We check if name is strictly equal to Mary.
+//    If it is, we console.log 'Hi, Mary!',
+//    otherwise we console.log 'How do you do?'.
+
 
 //CODE
 /*
@@ -45,10 +46,14 @@ if (name === 'Mary') {
 */
 
 // 2. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+//    We make a variable called secret, with no initialization value.
+//    We make a variable called code and set it's value to the number 123.
+//    We check if code is strictly equal to the number 123.
+//    If it is, we set the value of secret to the string 'super',
+//    and we double the value of code, overwriting the value of code to the number 246.
+//    We check if the value of code is greater than the number 250. (It is not.)
+//    If it is, we overwrite the value of secret, setting it to 'duper'. (It is not, so we don't)
+//    We console.log the value of secret, which is still 'super'.
 
 //CODE
 /*
@@ -70,10 +75,18 @@ console.log(secret)
 */
 
 // 3. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+//    We make a variable called isStudent and set it to true.
+//    We make a variable called age and set it to the number 34.
+//    We make a variable called zip and set it to the number 55407.
+//    if isStudent is strictly equal to true (it is) AND zip is greater than 80000 (it's not),
+//    we console.log 'You're a student on the West Coast!', (we don't)
+//    otherwise if isStudent is strictly equal to false (it's not) OR age is less than 30 (it's not),
+//    we console.log 'What are your hobbies?', (we don't)
+//    otherwise if isStudent is strictly equal to true (it is),
+//    we console.log 'Welcome to Prime!', (we do!  Then we peace out!)
+//    otherwise, if none of the above conditionals were met (one was), we console.log 'How about the weather?' (we don't).
+
+
 
 //CODE
 /*
@@ -113,9 +126,9 @@ if (isStudent === true && zip > 80000 ) {
 /*
 let number = 1;
 
-// FIX - number-- decrements number, but the instructions ask to increment. 
+// FIX - number-- decrements number, but the instructions ask to increment.
 // Should be number++.
-number--; 
+number--;
 
 if (number >= 2) {
   console.log('yes');
@@ -128,6 +141,12 @@ if (number >= 2) {
 //4. We start with three variables -- colorOne is set to 'blue' and 
 // colorTwo is set to 'red', and mix is set to true. We check if mix is true 
 // -- it is, so we set colorOne and colorTwo to 'purple'
+
+// FIX - 
+// colorOne and colorTwo are set to 'red' and 'blue', respectively, we should
+// swap their values.
+// inside the if conditional, we only assign colorOne to 'purple', but we should
+// also assign colorTwo to 'purple'.
 
 /*
 let colorOne = 'red';
@@ -143,6 +162,10 @@ if (mix === true) {
 // We check if temp is higher than 39 and if time is greater or equal to  4 
 // -- they are so we console.log 'throw away the food!'
 
+// FIX - 
+// We initialize time as a constant, we should use var instead of const.
+// We should use the logical AND operator (&&) instead of the logical OR operator (||).
+
 /*
 let temp = 40;
 const time = 4;
@@ -155,6 +178,10 @@ if (temp > 39 || time >= 4) {
 //6. We start with two variables -- age is set to 21 and minAge is set to 21.
 // We check if age is greater than or equal to minAge 
 // -- it is, so we console.log 'enter'.
+
+// FIX - 
+// We initialize minAge as a constant, we should use var instead of const.
+// The results of our branching if logic are reversed, we should swap the console.log statements.
 
 /*
 let age = 21;
